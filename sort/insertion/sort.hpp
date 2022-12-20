1dc07159d37e73459be8101d0665bfadaee88036
Alan Jian <alanjian85@outlook.com>
2022-12-20 20:21:30 +0800

    Add comments in the insertion sort

--- sort/insertion/sort.hpp
+++ sort/insertion/sort.hpp
@@ -1,11 +1,17 @@
 #ifndef INSERTION_HPP
 #define INSERTION_HPP
 
+// insertion sort
 template <typename T>
 void sort(T& a) {
+    // Get the array's signed size before starting the sorting process. Because
+    // the variable j derived from it on line 12 may become less than zero, n
+    // must be signed in order to avoid arithmetic underflow.
     auto n = std::ssize(a);
     for (decltype(n) i = 1; i < n; ++i) {
         auto key = a[i];
+        // Check if there is any element in the sorted sub-array is greater
+        // than the current key. And insert the key to an appropriate position. 
         auto j = i - 1;
         while (j >= 0 && a[j] > key) {
             a[j + 1] = a[j];
@@ -13,6 +19,7 @@ void sort(T& a) {
         }
         a[j + 1] = key;
     }
+    // After the termination, the loop invariant is still true
 }
 
 #endif // INSERTION_HPP
--- sort/sort.cpp
+++ sort/sort.cpp
@@ -8,18 +8,22 @@
 int main() {
     using element_t = int;
 
+    // Fetch a line from stdin
     std::cout << "Enter the array to be sorted: ";
     std::string line;
     std::getline(std::cin, line);
 
+    // Convert the retrieved line to an array of integers
     std::istringstream stream(line);
     std::vector<element_t> vector;
     for (element_t i; stream >> i;) {
         vector.push_back(i);
     }
 
+    // Sort the stored integers
     sort(vector);
 
+    // Output the sorted values
     std::cout << "The array is sorted: ";
     for (auto i : vector) {
         std::cout << i << ' ';
