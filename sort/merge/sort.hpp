f577a80a9e98ef99e41d36a6336a16edceb924b2
Alan Jian <alanjian85@outlook.com>
2022-12-22 23:23:28 +0800

    Add comment to merge sort

--- sort/merge/sort.hpp
+++ sort/merge/sort.hpp
@@ -32,16 +32,31 @@ void merge(T& a, decltype(std::size(a)) p, decltype(std::size(a)) q,
         a[p] = ar[j];
 }
 
+// The argument r's default value is set to 1. When the sandbox calls it with a
+// single argument A, p and r are filled in as 0 and -1, respectively. And once
+// the first two line of the procedure have been executed, r will be replaced
+// by the index of the last element in the array, indicating the range of the
+// entire array [0, N].
 template <typename T>
 void sort(T& a, decltype(std::size(a)) p = 0, decltype(std::ssize(a)) r = -1)
 {
     if (r == -1)
         r = std::ssize(a) - 1;
+    // If p = r, there is no need to sort, because an array with only one
+    // element is always sorted
+    // If p > r, it means the arguments don't form a valid range, so the
+    // procedure should return immediately
     if (p >= r)
         return;
+    // q is the sum of p and r shifted one bit to the right(divided by two),
+    // indicating that it is an average or a median of p and r. This is the
+    // "divide" stage of merge sort.
     auto q = (p + r) >> 1;
+    // Conquer the left portion and the right portion of the interval,
+    // separately
     sort(a, p, q);
     sort(a, q + 1, r);
+    // Combine the results
     merge(a, p, q, r);
 }
 
